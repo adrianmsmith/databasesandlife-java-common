5596bfab8faf7ddf07b9eef67acae0778ba16965
adrian <adrian@03946e4b-032c-0410-9851-fccc0fee27f7>
2009-03-28 16:37:55 +0000

    git-svn-id: https://svn.databasesandlife.com/data/trunk/dev/common/java@1515 03946e4b-032c-0410-9851-fccc0fee27f7

--- src/com/databasesandlife/Timer.java
+++ src/com/databasesandlife/Timer.java
@@ -7,7 +7,14 @@ package com.databasesandlife;
  */
 public class Timer {
 
-    public static long measureWallclockMilliseconds(Runnable task, long minimumDurationMillis) {
+    public static class TimerResult {
+        long millis;
+        int iterPerSecond;
+
+        public String toString() { return String.format("%.3f seconds (%d per second)", millis/1000.0, iterPerSecond); }
+    }
+
+    public static TimerResult measureWallclockMilliseconds(Runnable task, long minimumDurationMillis) {
         long startTime = System.currentTimeMillis();
         long endTime = startTime + minimumDurationMillis;
         int iterations = 0;
@@ -15,7 +22,11 @@ public class Timer {
             iterations++;
             task.run();
         }
-        long duration = System.currentTimeMillis() - endTime;
-        return duration / iterations;
+        long durationMillis = System.currentTimeMillis() - endTime;
+        
+        TimerResult result = new TimerResult();
+        result.millis = durationMillis / iterations;
+        result.iterPerSecond = (int) (1000L * iterations / durationMillis);
+        return result;
     }
 }
