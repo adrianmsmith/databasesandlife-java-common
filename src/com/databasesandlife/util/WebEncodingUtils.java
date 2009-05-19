5343cd634a618fb638de18133be36cc2b4d96882
adrian <adrian@03946e4b-032c-0410-9851-fccc0fee27f7>
2009-05-19 16:46:21 +0000

    git-svn-id: https://svn.databasesandlife.com/opensource/databasesandlife-java-common/trunk@1586 03946e4b-032c-0410-9851-fccc0fee27f7

---
+++ src/com/databasesandlife/util/WebEncodingUtils.java
@@ -0,0 +1,15 @@
+package com.databasesandlife.util;
+
+/**
+ * @author Adrian Smith
+ */
+public class WebEncodingUtils {
+
+    public static String encodeHtml(String x) {
+        x = x.replaceAll("&", "&amp;");
+        x = x.replaceAll(">", "&gt;");
+        x = x.replaceAll("<", "&lt;");
+        return x;
+    }
+
+}
