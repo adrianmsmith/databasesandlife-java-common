ea4b17ff8bf7455e47c60046b4c196ec012d6fa1
adrian <adrian@03946e4b-032c-0410-9851-fccc0fee27f7>
2009-12-23 22:57:57 +0000

    * StringsFile done to save storing C snippets in core; ProfileParser updated * New ProfileDesc classes done; ProfileParser updated * Made a start on the class to write the C file
    git-svn-id: https://svn.databasesandlife.com/opensource/databasesandlife-java-common/trunk@1896 03946e4b-032c-0410-9851-fccc0fee27f7

---
+++ src/com/databasesandlife/util/StringsFile.java
@@ -0,0 +1,77 @@
+package com.databasesandlife.util;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+/**
+ * A strings file is a place where Strings can be stored out of the JVM.
+ * If we are dealing with a 4GB XML file, we don't want all those strings hanging around core otherwise core will become full.
+ *    <p>
+ * Usage:
+ * <pre>
+ *     StringsFile file = new StringsFile();            // file created in /tmp
+ *     StringInFile str = file.newString("abc");        
+ *     System.out.println("str is: " + str.toString()); // fetches string from file
+ * </pre>
+ *
+ * @author Adrian Smith
+ */
+public class StringsFile {
+
+    RandomAccessFile file;
+
+    public class StringCannotBeAppended extends RuntimeException { StringCannotBeAppended() { } }
+
+    public class StringInFile {
+        protected long offset;
+        protected int byteLength;
+
+        public String toString() { return readString(this); }
+        public void append(String x) { appendToString(this, x); }
+    }
+
+    public StringsFile() {
+        try {
+            File f = File.createTempFile("StringsFile-", ".dat");
+            file = new RandomAccessFile(f, "rw");
+        }
+        catch (IOException e) { throw new RuntimeException(e); }
+    }
+
+    public synchronized StringInFile newString(String str) {
+        try {
+            StringInFile result = new StringInFile();
+            result.offset = file.length();
+            file.seek(result.offset);
+            byte[] bytes = str.getBytes("UTF-8");
+            file.write(bytes);
+            result.byteLength = bytes.length;
+            return result;
+        }
+        catch (IOException e) { throw new RuntimeException(e); }
+    }
+
+    /** @throws StringCannotBeAppended if string is not at end of file */
+    public synchronized void appendToString(StringInFile s, String suffix) throws StringCannotBeAppended {
+        try {
+            if (s.offset + s.byteLength != file.length()) throw new StringCannotBeAppended();
+            file.seek(file.length());
+            byte[] suffixBytes = suffix.getBytes("UTF-8");
+            file.write(suffixBytes);
+            s.byteLength += suffixBytes.length;
+        }
+        catch (IOException e) { throw new RuntimeException(e); }
+    }
+
+    public synchronized String readString(StringInFile s) {
+        try {
+            file.seek(s.offset);
+            byte[] resultBytes = new byte[s.byteLength];
+            int bytesRead = file.read(resultBytes);
+            if (bytesRead != s.byteLength) throw new RuntimeException("Could not read entire string");
+            return new String(resultBytes, "UTF-8");
+        }
+        catch (IOException e) { throw new RuntimeException(e); }
+    }
+}
---
+++ test-src/com/databasesandlife/util/StringsFileTest.java
@@ -0,0 +1,25 @@
+package com.databasesandlife.util;
+
+import com.databasesandlife.util.StringsFile.StringInFile;
+import junit.framework.TestCase;
+
+/**
+ * @author Adrian Smith
+ */
+public class StringsFileTest extends TestCase {
+    
+    public StringsFileTest(String testName) {
+        super(testName);
+    }
+
+    public void test() {
+        StringsFile str = new StringsFile();
+        StringInFile a = str.newString("abc");
+        StringInFile b = str.newString("foo\u20E0bar");
+        assertEquals("abc",    a.toString());
+        assertEquals("foo\u20E0bar", b.toString());
+        try { a.append("x"); fail(); } catch (StringsFile.StringCannotBeAppended e) { }
+        b.append("joe");
+        assertEquals("foo\u20E0barjoe", b.toString());
+    }
+}
