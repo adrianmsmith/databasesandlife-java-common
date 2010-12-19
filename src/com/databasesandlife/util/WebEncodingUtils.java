7ec3630c324ec4b899ffb72588cbc433aa88f86e
adrian <adrian@03946e4b-032c-0410-9851-fccc0fee27f7>
2010-12-19 18:21:21 +0000

    * ReportServlet implemented for Schoepf, including API documentation * Drop-down for cost-center takes into account privilege restrictions * Date drop-downs in calculation filter are now newest-to-oldest * CalculationDataFilter uses CostCenter object not string * CalculationDataFilter can be serialized/unserialized to Map * Directory re-structuring: wicket/ -> web/wicket/ * WebContext class to handle the commonalities between Wicket and Servlet * Documentation re-arranged into its own directory * Reporting JARs added
    git-svn-id: https://svn.databasesandlife.com/opensource/databasesandlife-java-common/trunk@2291 03946e4b-032c-0410-9851-fccc0fee27f7

--- src/com/databasesandlife/util/WebEncodingUtils.java
+++ src/com/databasesandlife/util/WebEncodingUtils.java
@@ -1,5 +1,11 @@
 package com.databasesandlife.util;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import javax.servlet.http.HttpServletRequest;
+
 /**
  * @author This source is copyright <a href="http://www.databasesandlife.com">Adrian Smith</a> and licensed under the LGPL 3.
  * @version $Revision$
@@ -15,4 +21,12 @@ public class WebEncodingUtils {
 
     // For GET parameters:
     // See http://java.sun.com/j2se/1.4.2/docs/api/java/net/URLEncoder.html
+
+	public static Map<String, String> getHttpRequestParameterMap(HttpServletRequest request) {
+		Map<String, String> result = new HashMap<String, String>();
+		for (Entry<String,String[]> paramEntry : ((Map<String,String[]>) request.getParameterMap()).entrySet())
+			result.put(paramEntry.getKey(), paramEntry.getValue()[0]);
+		return result;
+	}
+    
 }
