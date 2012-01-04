2a2d6efc070690f64244f7566809605cea37efb8
adrian <adrian@03946e4b-032c-0410-9851-fccc0fee27f7>
2012-01-04 11:58:51 +0000

    * Bug fix: Usage of DbMutex did an implicit commit
    git-svn-id: https://svn.databasesandlife.com/opensource/databasesandlife-java-common/trunk@2951 03946e4b-032c-0410-9851-fccc0fee27f7

--- src/com/databasesandlife/util/jdbc/DbMutex.java
+++ src/com/databasesandlife/util/jdbc/DbMutex.java
@@ -1,8 +1,13 @@
 package com.databasesandlife.util.jdbc;
 
 /** 
- * Maintains a table full of mutexes as described 
- * <a href="http://www.databasesandlife.com/mysql-lock-tables-does-an-implicit-commit/">here</a> 
+ * Maintains a table full of mutexes as described
+ * <a href="http://www.databasesandlife.com/mysql-lock-tables-does-an-implicit-commit/">here</a>
+ *    <p>
+ * To use this, create this table:
+ * <pre>
+ *   CREATE TABLE mutex (name VARCHAR(100)) ENGINE=InnoDB;
+ * </pre> 
  */
 public class DbMutex {
     
@@ -11,9 +16,6 @@ public class DbMutex {
     public DbMutex(String name) { this.name = name; }
     
     public void acquire(DbClient db) {
-        try { db.doSqlAction("CREATE TABLE mutex (name VARCHAR(100)) ENGINE=InnoDB"); }
-        catch (RuntimeException e) { if ( ! e.getMessage().contains("Table 'mutex' already exists")) throw e; }
-        
         try { db.doSqlAction("INSERT INTO mutex SET name=?", name); }
         catch (DbClient.UniqueConstraintViolation e) { }
         
