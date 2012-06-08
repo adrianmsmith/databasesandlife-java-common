cd454d2cdda30b0c3ff75a04afd85f1b2e7669ff
adrian <adrian@03946e4b-032c-0410-9851-fccc0fee27f7>
2012-06-08 12:19:58 +0000

    * Added: PaginationWidget
    git-svn-id: https://svn.databasesandlife.com/opensource/databasesandlife-java-common/trunk@3584 03946e4b-032c-0410-9851-fccc0fee27f7

---
+++ src/com/databasesandlife/util/wicket/PaginationWidget.java
@@ -0,0 +1,44 @@
+package com.databasesandlife.util.wicket;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.wicket.behavior.AttributeAppender;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.link.Link;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.ListView;
+import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.Model;
+
+public class PaginationWidget extends Panel {
+    
+    public interface PaginationWidgetCallback extends Serializable {
+        public Link<?> newLinkToPageIdx(String wicketId, int pageIdx);
+    }
+    
+    public PaginationWidget(String wicketId, final int pageIdx, int resultCount, int resultsPerPage, final PaginationWidgetCallback c) {
+        super(wicketId);
+
+        int maxPageIdxIncl = (resultCount - 1) / resultsPerPage;
+        List<Integer> pages = new ArrayList<Integer>();
+        for (int p = pageIdx - 10; p < pageIdx + 10; p++) {
+            if (p < 0) continue;
+            if (p > maxPageIdxIncl) continue;
+            pages.add(p);
+        }
+        add(c.newLinkToPageIdx("pagination.prev", pageIdx - 1).setVisible(pageIdx > 0));
+        add(c.newLinkToPageIdx("pagination.next", pageIdx + 1).setVisible(pageIdx < maxPageIdxIncl));
+        
+        add(new ListView<Integer>("pagination", pages) {
+            protected void populateItem(ListItem<Integer> item) {
+                int destPageIdx = item.getModelObject();
+                Link<?> link = c.newLinkToPageIdx("link", pageIdx); 
+                if (destPageIdx == pageIdx) link.add(new AttributeAppender("class", new Model<String>("active"), " "));
+                link.add(new Label("pageNo", "" + (1 + destPageIdx)));
+                item.add(link);
+            } 
+        });
+    }
+}
