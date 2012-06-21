92ebc9e8ab4fc6ffa192d650028ebc91318fe3c9
adrian <adrian@03946e4b-032c-0410-9851-fccc0fee27f7>
2012-06-21 18:18:04 +0000

    * Removed unused imports
    git-svn-id: https://svn.databasesandlife.com/opensource/databasesandlife-java-common/trunk@3623 03946e4b-032c-0410-9851-fccc0fee27f7

--- src/contrib/BCrypt.java
+++ src/contrib/BCrypt.java
@@ -14,7 +14,6 @@ package contrib;
 // ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
 // OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
