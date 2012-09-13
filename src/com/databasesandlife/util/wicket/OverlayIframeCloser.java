5e18f80c7caa3750421ad85f23d4850e6b1e7397
adrian <adrian@03946e4b-032c-0410-9851-fccc0fee27f7>
2012-09-13 08:57:46 +0000

    * OverlayIframeCloser can either close the overlay (if same domain) or not (if not)
    git-svn-id: https://svn.databasesandlife.com/opensource/databasesandlife-java-common/trunk@3786 03946e4b-032c-0410-9851-fccc0fee27f7

--- src/com/databasesandlife/util/wicket/OverlayIframeCloser.java
+++ src/com/databasesandlife/util/wicket/OverlayIframeCloser.java
@@ -1,17 +1,20 @@
 package com.databasesandlife.util.wicket;
 
+import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.link.Link;
 
 /** 
- * Breaks out of an iframe JQuery overlay ("colorbox"), and redirects the main browser window to a destination page.
- * 
+ * Breaks out of an iframe JQuery overlay ("colorbox" or "fancybox"), and redirects the main browser window to a destination page.
+ * <p>
+ * If the iframe is on the same domain as the parent then specify {@link DomainSimilarity#SameDomain} and the overlay is closed first (it looks nice), 
+ * otherwise specify {@link DomainSimilarity#DifferentDomain} and it isn't (as the browser won't allow it due to cross-domain concerns.)
  * <p>
  * Usage:
  * <pre>
  *     Link<?> linkToResult = new BookmarkablePageLink("link", ...);
- *     setResponsePage(new OverlayIframeCloser(linkToResult));
- * </pre>
+ *     setResponsePage(new OverlayIframeCloser(
+ *       DomainSimilarity.SameDomain, linkToResult)); </pre>
  * </p>
  *
  * @author The Java source is copyright <a href="http://www.databasesandlife.com">Adrian Smith</a> and licensed under the LGPL 3.
@@ -19,8 +22,11 @@ import org.apache.wicket.markup.html.link.Link;
  */
 public class OverlayIframeCloser extends WebPage {
     
+    public enum DomainSimilarity { SameDomain, DifferentDomain }
+    
     /** @param destinationPage wicket id of this link should be "link" */
-    public OverlayIframeCloser(Link<?> destinationPage) {
+    public OverlayIframeCloser(DomainSimilarity domainSimilarity, Link<?> destinationPage) {
         add(destinationPage);
+        add(new WebMarkupContainer("closeOverlayScript").setVisible(domainSimilarity == DomainSimilarity.SameDomain));
     }
 }
