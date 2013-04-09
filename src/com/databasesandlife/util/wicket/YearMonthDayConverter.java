e3766ccb9991e85e3c695886877742aa94ea18a1
adrian <adrian@03946e4b-032c-0410-9851-fccc0fee27f7>
2013-04-09 08:26:45 +0000

    * Moved YearMonthDayConverter from usage-dwh to databasesandlife-java-common (need it in job-website ;-)
    git-svn-id: https://svn.databasesandlife.com/opensource/databasesandlife-java-common/trunk@4620 03946e4b-032c-0410-9851-fccc0fee27f7

---
+++ src/com/databasesandlife/util/wicket/YearMonthDayConverter.java
@@ -0,0 +1,58 @@
+package com.databasesandlife.util.wicket;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
+import org.apache.wicket.ConverterLocator;
+import org.apache.wicket.IConverterLocator;
+import org.apache.wicket.util.convert.ConversionException;
+import org.apache.wicket.util.convert.IConverter;
+
+import com.databasesandlife.util.YearMonthDay;
+
+/**
+ * Allows Wicket fields to have models of type {@link YearMonthDay}.
+ *    <p>
+ * To use, for example add the following method to the Wicket application:
+ * <pre>
+ * protected IConverterLocator newConverterLocator() {
+ *   ConverterLocator locator = 
+ *     (ConverterLocator) super.newConverterLocator();
+ *   locator.set(YearMonthDay.class, 
+ *     new YearMonthDayConverter(new SimpleDateFormat("dd.MM.yyyy")));
+ *   return locator;
+ * }
+ * </pre>
+ */
+public class YearMonthDayConverter implements IConverter<YearMonthDay>{
+
+	private final SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+	private SimpleDateFormat userFormat;
+	
+	public YearMonthDayConverter(SimpleDateFormat format){
+		userFormat = format;
+	}
+	
+	@Override
+	public YearMonthDay convertToObject(String arg0, Locale arg1) {
+		if(arg0 == null) return null;
+		try {
+			return YearMonthDay.newForYYYYMMDD(format.format(userFormat.parse(arg0)));
+		} catch (ParseException e) {
+			throw new ConversionException(e).setResourceKey("YearMonthDay not valid");
+		}
+	}
+
+	@Override
+	public String convertToString(YearMonthDay arg0, Locale arg1) {
+		if(arg0 == null) return null;
+ 		try {
+			return userFormat.format(format.parse(arg0.toYYYYMMDD()));
+		} catch (ParseException e) {
+			throw new ConversionException(e).setResourceKey("YearMonthDay not valid");
+		}
+	}
+
+
+}
