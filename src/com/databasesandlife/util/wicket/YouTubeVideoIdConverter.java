0e45fd372474947b324e31b6b21d6d9888933d61
adrian <adrian@03946e4b-032c-0410-9851-fccc0fee27f7>
2013-04-12 09:46:23 +0000

    * Generics
    git-svn-id: https://svn.databasesandlife.com/opensource/databasesandlife-java-common/trunk@4648 03946e4b-032c-0410-9851-fccc0fee27f7

--- src/com/databasesandlife/util/wicket/YouTubeVideoIdConverter.java
+++ src/com/databasesandlife/util/wicket/YouTubeVideoIdConverter.java
@@ -14,9 +14,9 @@ import com.databasesandlife.util.YouTubeVideoId;
  * @author The Java source is copyright <a href="http://www.databasesandlife.com">Adrian Smith</a> and licensed under the LGPL 3.
  * @version $Revision$
  */
-public class YouTubeVideoIdConverter implements IConverter {
+public class YouTubeVideoIdConverter implements IConverter<YouTubeVideoId> {
 
-    @Override public Object convertToObject(String value, Locale locale) {
+    @Override public YouTubeVideoId convertToObject(String value, Locale locale) {
         Matcher m;
         
         if (value == null || "".equals(value)) return null;
@@ -30,7 +30,7 @@ public class YouTubeVideoIdConverter implements IConverter {
         throw new ConversionException("URL '" + value + "' doesn't look like a youtube URL");
     }
 
-    @Override public String convertToString(Object value, Locale locale) {
+    @Override public String convertToString(YouTubeVideoId value, Locale locale) {
         YouTubeVideoId obj = (YouTubeVideoId) value;
         if (obj == null) return "";
         return "http://www.youtube.com/watch?v=" + obj.id;
