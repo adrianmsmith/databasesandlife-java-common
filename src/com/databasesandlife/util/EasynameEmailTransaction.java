e3540f6639a9b41eebab0f2b9bfddad023cd52af
adrian <adrian@03946e4b-032c-0410-9851-fccc0fee27f7>
2013-12-15 14:41:31 +0000

    * Added: EasynameEmailTransaction
    git-svn-id: https://svn.databasesandlife.com/opensource/databasesandlife-java-common/trunk@6302 03946e4b-032c-0410-9851-fccc0fee27f7

---
+++ src/com/databasesandlife/util/EasynameEmailTransaction.java
@@ -0,0 +1,41 @@
+package com.databasesandlife.util;
+
+import java.util.Properties;
+
+import javax.mail.Authenticator;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.internet.MimeMessage;
+
+/**
+ * Sends emails via easyname.eu.
+ */
+public class EasynameEmailTransaction extends EmailTransaction {
+    
+    protected final String emailBoxName;
+    protected final String emailBoxPasswordCleartext;
+    
+    /** @param emailBoxName for example "123mail1" */
+    public EasynameEmailTransaction(String emailBoxName, String emailBoxPasswordCleartext) {
+        super(null);
+        this.emailBoxName = emailBoxName;
+        this.emailBoxPasswordCleartext = emailBoxPasswordCleartext;
+    }
+    
+    @Override public MimeMessage newMimeMessage() {
+        Properties props = new Properties();
+        props.put("mail.smtp.host", "smtp.easyname.eu");
+        props.put("mail.smtp.socketFactory.port", "465");
+        props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.port", "465");
+     
+        Authenticator auth = new Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(emailBoxName, emailBoxPasswordCleartext);
+            }
+        };
+        Session session = Session.getDefaultInstance(props, auth);
+        return new MimeMessage(session);
+    }
+}
