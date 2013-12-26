a96ad7d176e56c34ee7b13870b2cfd2e7229c52d
adrian <adrian@03946e4b-032c-0410-9851-fccc0fee27f7>
2013-12-26 16:28:58 +0000

    * Added: EmptyListIsSpecialWrapperModel (extracted from usage-dwh CalculationDataFilterWidget)
    git-svn-id: https://svn.databasesandlife.com/opensource/databasesandlife-java-common/trunk@6359 03946e4b-032c-0410-9851-fccc0fee27f7

---
+++ src/com/databasesandlife/util/wicket/EmptyListIsSpecialWrapperModel.java
@@ -0,0 +1,39 @@
+package com.databasesandlife.util.wicket;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+import org.apache.wicket.model.IModel;
+
+/**
+ * If the user enters greater than zero options, they are passed to the wrapped model.
+ * Otherwise, if the user doesn't enter any options, some "default" list is set in the wrapped model.
+ * The "default" list may be null. 
+ */
+@SuppressWarnings("serial")
+public class EmptyListIsSpecialWrapperModel<T> implements IModel<List<T>> {
+    protected List<T> emptyValues;
+    protected IModel<List<T>> dataModel;
+    
+    /** @param e which values to be saved in the underlying model, in the case the user doesn't enter anything */
+    public EmptyListIsSpecialWrapperModel(List<T> e, IModel<List<T>> m) { 
+        emptyValues = e;
+        dataModel=m; 
+    }
+    
+    @Override public void detach() { }
+    
+    @Override public void setObject(List<T> userData) { 
+        dataModel.setObject(userData.isEmpty() ? emptyValues : userData); 
+    }
+    
+    /** Returns data to user i.e. show the user empty list, if model = empty values */
+    @Override public List<T> getObject() {
+        List<T> dataValues = dataModel.getObject();
+        if (dataValues == null && emptyValues == null) return new ArrayList<T>();
+        if (dataValues == null || emptyValues == null) return dataValues; // not the same if only one is null
+        if (new HashSet<T>(dataValues).equals(new HashSet<T>(emptyValues))) return new ArrayList<T>();
+        return dataValues;
+    }
+}
