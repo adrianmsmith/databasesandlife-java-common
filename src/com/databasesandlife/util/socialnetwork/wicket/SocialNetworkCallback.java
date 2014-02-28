6f1cfcd0b6e3b1fbb32cd948b42607c7ff40ba5f
adrian <adrian@03946e4b-032c-0410-9851-fccc0fee27f7>
2014-02-28 11:02:22 +0000

    * Extended doc on SocialNetworkCallback
    git-svn-id: https://svn.databasesandlife.com/opensource/databasesandlife-java-common/trunk@6608 03946e4b-032c-0410-9851-fccc0fee27f7

--- src/com/databasesandlife/util/socialnetwork/wicket/SocialNetworkCallback.java
+++ src/com/databasesandlife/util/socialnetwork/wicket/SocialNetworkCallback.java
@@ -2,9 +2,18 @@ package com.databasesandlife.util.socialnetwork.wicket;
 
 import java.io.Serializable;
 
+import org.apache.wicket.PageReference;
+
 import com.databasesandlife.util.socialnetwork.SocialNetworkToken;
 
 
+/** 
+ * Methods to be called when authorization is accepted or rejected by a social network.
+ *    <p>
+ * <b>NOTE:</b> Objects implementing this <b>should not be</b> (or reference) <b>pages</b>.
+ * (As these objects are stored in pages, and pages should not reference other pages.)
+ * If you need to reference a page, use {@link PageReference}. 
+ */
 public interface SocialNetworkCallback extends Serializable{
 
 	public void onAuthentication(SocialNetworkToken networkToken);
