b80b5aaf8f8044db77dae1ca389d19a98695a63f
adrian <adrian@03946e4b-032c-0410-9851-fccc0fee27f7>
2014-03-10 07:38:29 +0000

    * Added: MultilineLabelWithClickableLinks
    git-svn-id: https://svn.databasesandlife.com/opensource/databasesandlife-java-common/trunk@6667 03946e4b-032c-0410-9851-fccc0fee27f7

---
+++ src-test/com/databasesandlife/util/wicket/MultilineLabelWithClickableLinksTest.java
@@ -0,0 +1,30 @@
+package com.databasesandlife.util.wicket;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import junit.framework.TestCase;
+
+public class MultilineLabelWithClickableLinksTest extends TestCase {
+    
+    public void testLinkPattern() {
+        Pattern p = MultilineLabelWithClickableLinks.linkPattern;
+        Matcher m;
+        assertEquals("foo.com", (m = p.matcher("a foo.com. b")).find() ? m.group() : "");
+        assertEquals("foo-at-end.com", (m = p.matcher("a foo-at-end.com")).find() ? m.group() : "");
+        assertEquals("hyphen-hyphen.com", (m = p.matcher("a hyphen-hyphen.com. b")).find() ? m.group() : "");
+        assertEquals("numbers123.com", (m = p.matcher("a numbers123.com. b")).find() ? m.group() : "");
+        assertEquals("UPPERCASE.com", (m = p.matcher("a UPPERCASE.com. b")).find() ? m.group() : "");
+        assertEquals("foo.foo.com", (m = p.matcher("a foo.foo.com. b")).find() ? m.group() : "");
+        assertEquals("foo://foo.com", (m = p.matcher("a foo://foo.com. b")).find() ? m.group() : "");
+        assertEquals("foo://foo.com/", (m = p.matcher("a foo://foo.com/. b")).find() ? m.group() : "");
+        assertEquals("foo://foo.com/1", (m = p.matcher("a foo://foo.com/1. b")).find() ? m.group() : "");
+        assertEquals("foo://foo.com/dot.dot", (m = p.matcher("a foo://foo.com/dot.dot. b")).find() ? m.group() : "");
+    }
+
+    public void testEncodeLinksToHtml() {
+        String subject = "< foo.com/foo&bar >";
+        String expected = "&lt; <a target=_blank href='foo.com/foo&bar'>foo.com/foo&amp;bar</a> &gt;";
+        assertEquals(expected, MultilineLabelWithClickableLinks.encodeLinksToHtml(subject).toString());
+    }
+}
