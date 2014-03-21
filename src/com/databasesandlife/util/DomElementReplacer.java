d0a2a155527ff6ab202a8d5f5b4527a94668f0f0
adrian <adrian@03946e4b-032c-0410-9851-fccc0fee27f7>
2014-03-21 08:37:01 +0000

    git-svn-id: https://svn.databasesandlife.com/opensource/databasesandlife-java-common/trunk@6699 03946e4b-032c-0410-9851-fccc0fee27f7

--- src/com/databasesandlife/util/DomElementReplacer.java
+++ src/com/databasesandlife/util/DomElementReplacer.java
@@ -1,17 +1,39 @@
 package com.databasesandlife.util;
 
+import org.w3c.dom.Attr;
+import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.NamedNodeMap;
 
 /**
  * Takes a DOM and an element name; 
  * each time that element name is found it is replaced with a specified other element (which may contain other elements).
+ *    <p>
+ * The element name, that is seached for, may exist in any namespace.
+ * The replaced element is changed to be within the namespace of the found element, before insertion
+ * in the destination document.
  */
 public class DomElementReplacer extends DomParser {
+    
+    public static Element cloneElementAndSetNamespace(Document destDocument, Element source, String newNamespaceUri) {
+        Element result = destDocument.createElementNS(newNamespaceUri, source.getNodeName());
+        
+        NamedNodeMap at = source.getAttributes();
+        for (int a = 0; a < at.getLength(); a++) {
+            Attr att = (Attr) at.item(a);
+            result.setAttribute(att.getNodeName(), att.getValue());
+        }
+        
+        for (Element child : getSubElements(source, "*"))
+            result.appendChild(cloneElementAndSetNamespace(destDocument, child, newNamespaceUri));
+        
+        return result;
+    }
     
     public static void replace(Element doc, String elementName, Element replace) {
         for (Element e : getSubElements(doc, "*")) {
             if (e.getNodeName().equals(elementName)) {
-                Element r = (Element) doc.getOwnerDocument().importNode(replace, true);
+                Element r = cloneElementAndSetNamespace(doc.getOwnerDocument(), replace, e.getNamespaceURI());
                 doc.insertBefore(r, e);
                 doc.removeChild(e);
             } 
