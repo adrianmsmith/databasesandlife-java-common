0c102c950d551ecb24d502a8634e2bf040d1a5fd
asmith <asmith@03946e4b-032c-0410-9851-fccc0fee27f7>
2015-08-05 11:15:38 +0000

    git-svn-id: https://svn.databasesandlife.com/opensource/databasesandlife-java-common/trunk@8275 03946e4b-032c-0410-9851-fccc0fee27f7

--- src/com/databasesandlife/util/EmailTransaction.java
+++ src/com/databasesandlife/util/EmailTransaction.java
@@ -15,12 +15,15 @@ import javax.mail.internet.MimeMessage;
 /**
  * An transaction allowing emails to be sent.
  *   <p>
- * This object allows emails to take part in transactional operations.
+ * This object allows emails to take part in <b>transactional</b> operations.
  * Any part of the code may {@link #send(Message)} emails using this transaction.
  * They get stored in memory, and only really get sent when {@link #commit()} is called.
  * This way, if an exception occurs after the email gets sent, and the operation is successfully
  * retried, the recipient only gets one email based on the successful operation and no emails
  * based on the unsuccessful operation.
+ *   <p>
+ * There is no need to <b>rollback</b> this object if an operation has been not successful.
+ * Simply throw the object away.
  */
 public class EmailTransaction {
     
@@ -43,16 +46,12 @@ public class EmailTransaction {
     }
     
     public void commit() {
-        Timer.start("EmailTransaction-commit");
-        try {
-            for (Message msg : messages) {
-                Timer.start("Send email to '" + msg.getRecipients(RecipientType.TO)[0]+"'");
-                try { Transport.send(msg); }
-                finally { Timer.end("Send email to '" + msg.getRecipients(RecipientType.TO)[0]+"'"); }
+        for (Message msg : messages) {
+            try (Timer t = new Timer("EmailTransaction-commit: Send email to '" + msg.getRecipients(RecipientType.TO)[0]+"'")) {
+                Transport.send(msg);
             }
+            catch (MessagingException e) { throw new RuntimeException(e); }
         }
-        catch (MessagingException e) { throw new RuntimeException(e); }
-        finally { Timer.end("EmailTransaction-commit"); }
     }
     
     public int getEmailCountForTesting() { return messages.size(); }
