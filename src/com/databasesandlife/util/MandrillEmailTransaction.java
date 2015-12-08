01b609feccd8b6687d682e8ed721ede22e8de775
akichev <akichev@03946e4b-032c-0410-9851-fccc0fee27f7>
2015-12-08 21:29:12 +0000

    Fixed email from the webserver bug
    git-svn-id: https://svn.databasesandlife.com/opensource/databasesandlife-java-common/trunk@9267 03946e4b-032c-0410-9851-fccc0fee27f7

--- src/com/databasesandlife/util/MandrillEmailTransaction.java
+++ src/com/databasesandlife/util/MandrillEmailTransaction.java
@@ -7,10 +7,10 @@ import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 
 public class MandrillEmailTransaction extends EmailTransaction {
-    
+
     protected final String username;
     protected final String apiKey;
-    
+
     public MandrillEmailTransaction(String username, String apiKey) {
         super("smtp.mandrillapp.com");
         this.username = username;
@@ -19,18 +19,20 @@ public class MandrillEmailTransaction extends EmailTransaction {
 
     @Override protected Properties newSessionProperties() {
         Properties props = super.newSessionProperties();
-        props.put("mail.smtp.auth", true);
+        props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
         props.put("mail.smtp.port", "587");
+        props.put("mail.debug", "true");
+        props.put("mail.transport.protocol", "smtp");
         return props;
     }
-    
+
     @Override protected Session newSession() {
         Authenticator auth = new Authenticator() {
             protected PasswordAuthentication getPasswordAuthentication() {
                 return new PasswordAuthentication(username, apiKey);
             }
         };
-        return Session.getDefaultInstance(newSessionProperties(), auth);
+        return Session.getInstance(newSessionProperties(), auth);
     }
 }
