676577d41a0839726a185528ee21cae37132e652
asmith <asmith@03946e4b-032c-0410-9851-fccc0fee27f7>
2016-02-26 17:35:24 +0000

    * Removed JavaSqlTimeConverter and integrated it into TimeTextField so you can always use <input type=time> without worrying about date formats etc.
    git-svn-id: https://svn.databasesandlife.com/opensource/databasesandlife-java-common/trunk@10362 03946e4b-032c-0410-9851-fccc0fee27f7

--- src/com/databasesandlife/util/wicket/TimeTextField.java
+++ src/com/databasesandlife/util/wicket/TimeTextField.java
@@ -1,10 +1,24 @@
 package com.databasesandlife.util.wicket;
 
+import java.sql.Time;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoField;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
+
 import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.model.IModel;
+import org.apache.wicket.util.convert.IConverter;
 
 /**
- * &lt;input type="time"&gt; field.
+ * &lt;input type="time"&gt; field. 
+ * 
+ * <p>Model may be {@link java.sql.Time} or Java 8 {@link java.time.LocalTime}.
+ * (Jodatime is not supported as this library is Java 8 and thus Java 8 time should be used in preference.) 
+ * 
+ * <p>{@link java.sql.Time} is adjusted with the timezone to make it compatible with PostgreSQL and jOOQ.
  * 
  * @author The Java source is copyright <a href="http://www.databasesandlife.com">Adrian Smith</a> and licensed under the LGPL 3.
  * @version $Revision: 8157 $
@@ -17,9 +31,32 @@ public class TimeTextField<T> extends TextField<T> {
     }
 
     @Override
-    protected String getInputType()
-    {
+    protected String getInputType() {
         return "time";
     }
+    
+    @SuppressWarnings("unchecked")
+    @Override
+    public <C> IConverter<C> getConverter(Class<C> type) {
+        if (type.equals(Time.class)) return (IConverter<C>) new IConverter<Time>() {
+            @Override public Time convertToObject(String str, Locale l) {
+                return new Time(LocalTime.parse(str, DateTimeFormatter.ofPattern("HH:mm")).get(ChronoField.MILLI_OF_DAY)
+                    - TimeZone.getDefault().getOffset(new Date().getTime()));
+            }
 
+            @Override public String convertToString(Time time, Locale l) {
+                long millisOfDay = time.getTime() + TimeZone.getDefault().getOffset(new Date().getTime());
+                return time == null ? "" : LocalTime.ofSecondOfDay(millisOfDay/1000).format(DateTimeFormatter.ofPattern("HH:mm"));
+            }
+        };
+
+        if (type.equals(LocalTime.class)) return (IConverter<C>) new IConverter<LocalTime>() {
+            @Override public LocalTime convertToObject(String str, Locale l) { 
+                return LocalTime.parse(str, DateTimeFormatter.ofPattern("HH:mm")); }
+            @Override public String convertToString(LocalTime time, Locale l) { 
+                return time.format(DateTimeFormatter.ofPattern("HH:mm")); }
+        };
+
+        return super.getConverter(type);
+    }
 }
