2efe4385f55d3093befff5f33f5932de085d5b38
asmith <asmith@03946e4b-032c-0410-9851-fccc0fee27f7>
2016-02-26 17:52:44 +0000

    * Added <input type=date> field
    git-svn-id: https://svn.databasesandlife.com/opensource/databasesandlife-java-common/trunk@10366 03946e4b-032c-0410-9851-fccc0fee27f7

---
+++ src/com/databasesandlife/util/wicket/DateTextField.java
@@ -0,0 +1,56 @@
+package com.databasesandlife.util.wicket;
+
+import java.sql.Date;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.util.convert.IConverter;
+
+/**
+ * &lt;input type="date"&gt; field. 
+ * 
+ * <p>Model may be {@link java.sql.Date} or Java 8 {@link java.time.LocalDate}.
+ * (Jodatime is not supported as this library is Java 8 and thus Java 8 time should be used in preference.) 
+ * 
+ * @author The Java source is copyright <a href="http://www.databasesandlife.com">Adrian Smith</a> and licensed under the LGPL 3.
+ * @version $Revision: 8157 $
+ */
+@SuppressWarnings("serial")
+public class DateTextField<T> extends TextField<T> {
+
+    public DateTextField(String id, IModel<T> model) {
+        super(id, model);
+    }
+
+    @Override
+    protected String getInputType() {
+        return "date";
+    }
+    
+    @SuppressWarnings("unchecked")
+    @Override
+    public <C> IConverter<C> getConverter(Class<C> type) {
+        if (type.equals(Date.class)) return (IConverter<C>) new IConverter<Date>() {
+            @Override public Date convertToObject(String str, Locale l) {
+                if (str == null) return null;
+                return Date.valueOf(LocalDate.parse(str, DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+            }
+
+            @Override public String convertToString(Date time, Locale l) {
+                if (time == null) return null;
+                return time.toLocalDate().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+            }
+        };
+
+        if (type.equals(LocalDate.class)) return (IConverter<C>) new IConverter<LocalDate>() {
+            @Override public LocalDate convertToObject(String str, Locale l) { 
+                return LocalDate.parse(str, DateTimeFormatter.ofPattern("yyyy-MM-dd")); }
+            @Override public String convertToString(LocalDate time, Locale l) { 
+                return time.format(DateTimeFormatter.ofPattern("yyyy-MM-dd")); }
+        };
+
+        return super.getConverter(type);
+    }
+}
