0a8b87a3a617eaa977ba67f95c695aee0ecc3711
asmith <asmith@03946e4b-032c-0410-9851-fccc0fee27f7>
2016-02-26 17:43:08 +0000

    * Simplified
    git-svn-id: https://svn.databasesandlife.com/opensource/databasesandlife-java-common/trunk@10364 03946e4b-032c-0410-9851-fccc0fee27f7

--- src/com/databasesandlife/util/wicket/TimeTextField.java
+++ src/com/databasesandlife/util/wicket/TimeTextField.java
@@ -18,8 +18,6 @@ import org.apache.wicket.util.convert.IConverter;
  * <p>Model may be {@link java.sql.Time} or Java 8 {@link java.time.LocalTime}.
  * (Jodatime is not supported as this library is Java 8 and thus Java 8 time should be used in preference.) 
  * 
- * <p>{@link java.sql.Time} is adjusted with the timezone to make it compatible with PostgreSQL and jOOQ.
- * 
  * @author The Java source is copyright <a href="http://www.databasesandlife.com">Adrian Smith</a> and licensed under the LGPL 3.
  * @version $Revision: 8157 $
  */
@@ -40,13 +38,13 @@ public class TimeTextField<T> extends TextField<T> {
     public <C> IConverter<C> getConverter(Class<C> type) {
         if (type.equals(Time.class)) return (IConverter<C>) new IConverter<Time>() {
             @Override public Time convertToObject(String str, Locale l) {
-                return new Time(LocalTime.parse(str, DateTimeFormatter.ofPattern("HH:mm")).get(ChronoField.MILLI_OF_DAY)
-                    - TimeZone.getDefault().getOffset(new Date().getTime()));
+                if (str == null) return null;
+                return Time.valueOf(LocalTime.parse(str, DateTimeFormatter.ofPattern("HH:mm")));
             }
 
             @Override public String convertToString(Time time, Locale l) {
-                long millisOfDay = time.getTime() + TimeZone.getDefault().getOffset(new Date().getTime());
-                return time == null ? "" : LocalTime.ofSecondOfDay(millisOfDay/1000).format(DateTimeFormatter.ofPattern("HH:mm"));
+                if (time == null) return null;
+                return time.toLocalTime().format(DateTimeFormatter.ofPattern("HH:mm"));
             }
         };
 
