8ec5b1ffdaae8c5b4aab644896b62cc4638a9fd2
asmith <asmith@03946e4b-032c-0410-9851-fccc0fee27f7>
2016-03-10 17:16:30 +0000

    * Fields for <input type="date"> and <input type="time"> have placeholder values and errors (not crashes) if invalid input is entered. Older browsers do not display the input fields specially, and just display a blank text field, leaving the user free to wonder what they should enter, and also to enter invalid values.
    git-svn-id: https://svn.databasesandlife.com/opensource/databasesandlife-java-common/trunk@10567 03946e4b-032c-0410-9851-fccc0fee27f7

--- src/com/databasesandlife/util/wicket/DateTextField.java
+++ src/com/databasesandlife/util/wicket/DateTextField.java
@@ -3,9 +3,13 @@ package com.databasesandlife.util.wicket;
 import java.sql.Date;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.Locale;
+
+import org.apache.wicket.markup.ComponentTag;
 import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.model.IModel;
+import org.apache.wicket.util.convert.ConversionException;
 import org.apache.wicket.util.convert.IConverter;
 
 /**
@@ -13,6 +17,10 @@ import org.apache.wicket.util.convert.IConverter;
  * 
  * <p>Model may be {@link java.sql.Date} or Java 8 {@link java.time.LocalDate}.
  * (Jodatime is not supported as this library is Java 8 and thus Java 8 time should be used in preference.) 
+ * 
+ * <p>For those browsers which do not support <code>&lt;input type="date"&gt;</code>, 
+ * which display a normal <code>&lt;input type="text"&gt;</code>
+ * instead, for example Firefox at the time of writing, we show a placeholder text, and display an error in case the format is wrong.
  * 
  * @author The Java source is copyright <a href="http://www.databasesandlife.com">Adrian Smith</a> and licensed under the LGPL 3.
  * @version $Revision$
@@ -29,13 +37,26 @@ public class DateTextField<T> extends TextField<T> {
         return "date";
     }
     
+    protected LocalDate parse(String str) {
+        try {
+            if (str == null) return null;
+            else return LocalDate.parse(str, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        }
+        catch (DateTimeParseException e) { 
+            throw new ConversionException(e)
+                .setResourceKey("DateTextField.invalid")
+                .setSourceValue(str)
+                .setVariable("today", LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+        }
+    }
+    
     @SuppressWarnings("unchecked")
     @Override
     public <C> IConverter<C> getConverter(Class<C> type) {
         if (type.equals(Date.class)) return (IConverter<C>) new IConverter<Date>() {
             @Override public Date convertToObject(String str, Locale l) {
                 if (str == null) return null;
-                return Date.valueOf(LocalDate.parse(str, DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+                return Date.valueOf(parse(str));
             }
 
             @Override public String convertToString(Date time, Locale l) {
@@ -46,11 +67,21 @@ public class DateTextField<T> extends TextField<T> {
 
         if (type.equals(LocalDate.class)) return (IConverter<C>) new IConverter<LocalDate>() {
             @Override public LocalDate convertToObject(String str, Locale l) { 
-                return LocalDate.parse(str, DateTimeFormatter.ofPattern("yyyy-MM-dd")); }
+                return parse(str);
+            }
+            
             @Override public String convertToString(LocalDate time, Locale l) { 
-                return time.format(DateTimeFormatter.ofPattern("yyyy-MM-dd")); }
+                return time.format(DateTimeFormatter.ofPattern("yyyy-MM-dd")); 
+            }
         };
 
         return super.getConverter(type);
+    }
+    
+    @Override
+    protected void onComponentTag(ComponentTag tag) {
+        super.onComponentTag(tag);
+        String placeholder = (String) tag.getAttributes().get("placeholder");
+        if (placeholder == null) tag.getAttributes().put("placeholder", "YYYY-MM-DD");
     }
 }
--- src/com/databasesandlife/util/wicket/TimeTextField.java
+++ src/com/databasesandlife/util/wicket/TimeTextField.java
@@ -3,16 +3,24 @@ package com.databasesandlife.util.wicket;
 import java.sql.Time;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.Locale;
+
+import org.apache.wicket.markup.ComponentTag;
 import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.model.IModel;
+import org.apache.wicket.util.convert.ConversionException;
 import org.apache.wicket.util.convert.IConverter;
 
 /**
- * &lt;input type="time"&gt; field. 
+ * <code>&lt;input type="time"&gt;</code> field. 
  * 
  * <p>Model may be {@link java.sql.Time} or Java 8 {@link java.time.LocalTime}.
  * (Jodatime is not supported as this library is Java 8 and thus Java 8 time should be used in preference.) 
+ * 
+ * <p>For those browsers which do not support <code>&lt;input type="time"&gt;</code>, 
+ * which display a normal <code>&lt;input type="text"&gt;</code>
+ * instead, for example Firefox at the time of writing, we show a placeholder text, and display an error in case the format is wrong.
  * 
  * @author The Java source is copyright <a href="http://www.databasesandlife.com">Adrian Smith</a> and licensed under the LGPL 3.
  * @version $Revision$
@@ -29,15 +37,24 @@ public class TimeTextField<T> extends TextField<T> {
         return "time";
     }
     
+    protected LocalTime parse(String str) {
+        try {
+            if (str == null) return null;
+            else return LocalTime.parse(str, DateTimeFormatter.ofPattern("HH:mm"));
+        }
+        catch (DateTimeParseException e) { 
+            throw new ConversionException(e).setResourceKey("TimeTextField.invalid").setSourceValue(str);
+        }
+    }
+    
     @SuppressWarnings("unchecked")
     @Override
     public <C> IConverter<C> getConverter(Class<C> type) {
         if (type.equals(Time.class)) return (IConverter<C>) new IConverter<Time>() {
-            @Override public Time convertToObject(String str, Locale l) {
+            @Override public Time convertToObject(String str, Locale l) { 
                 if (str == null) return null;
-                return Time.valueOf(LocalTime.parse(str, DateTimeFormatter.ofPattern("HH:mm")));
+                return Time.valueOf(parse(str)); 
             }
-
             @Override public String convertToString(Time time, Locale l) {
                 if (time == null) return null;
                 return time.toLocalTime().format(DateTimeFormatter.ofPattern("HH:mm"));
@@ -46,11 +63,21 @@ public class TimeTextField<T> extends TextField<T> {
 
         if (type.equals(LocalTime.class)) return (IConverter<C>) new IConverter<LocalTime>() {
             @Override public LocalTime convertToObject(String str, Locale l) { 
-                return LocalTime.parse(str, DateTimeFormatter.ofPattern("HH:mm")); }
+                return parse(str); 
+            }
             @Override public String convertToString(LocalTime time, Locale l) { 
-                return time.format(DateTimeFormatter.ofPattern("HH:mm")); }
+                if (time == null) return null;
+                return time.format(DateTimeFormatter.ofPattern("HH:mm")); 
+            }
         };
 
         return super.getConverter(type);
+    }
+    
+    @Override
+    protected void onComponentTag(ComponentTag tag) {
+        super.onComponentTag(tag);
+        String placeholder = (String) tag.getAttributes().get("placeholder");
+        if (placeholder == null) tag.getAttributes().put("placeholder", "HH:MM");
     }
 }
