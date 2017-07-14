b42a1a7b3cef5f58c079dce54f1761ab6e3652a1
Adrian Smith <adrian.m.smith@gmail.com>
2017-07-14 14:46:05 +0200

    Rename class to more accurately reflect its function

--- src/main/java/com/databasesandlife/util/jooq/UtcInstantConverter.java
+++ src/main/java/com/databasesandlife/util/jooq/UtcInstantConverter.java
@@ -2,12 +2,12 @@ package com.databasesandlife.util.jooq;
 
 import java.time.Instant;
 import java.time.LocalDateTime;
-import java.time.ZoneId;
+import java.time.ZoneOffset;
 
 import org.jooq.Converter;
 
 @SuppressWarnings("serial")
-public class InstantConverter implements Converter<LocalDateTime, Instant> {
+public class UtcInstantConverter implements Converter<LocalDateTime, Instant> {
 
     @Override public Class<LocalDateTime> fromType() { return LocalDateTime.class; }
     @Override public Class<Instant> toType() { return Instant.class; }
@@ -15,12 +15,12 @@ public class InstantConverter implements Converter<LocalDateTime, Instant> {
     @Override
     public Instant from(LocalDateTime when) {
         if (when == null) return null;
-        return when.atZone(ZoneId.of("UTC")).toInstant();
+        return when.toInstant(ZoneOffset.UTC);
     }
 
     @Override
     public LocalDateTime to(Instant when) {
         if (when == null) return null;
-        return when.atZone(ZoneId.of("UTC")).toLocalDateTime();
+        return when.atOffset(ZoneOffset.UTC).toLocalDateTime();
     }
 }
