5114c2302af4b0f776fc6fc77e486f13dc947cb5
Adrian Smith <adrian.m.smith@gmail.com>
2018-02-27 12:10:03 +0100

    Make CleartextPassword serializable

--- src/main/java/com/databasesandlife/util/CleartextPassword.java
+++ src/main/java/com/databasesandlife/util/CleartextPassword.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
 
 import javax.annotation.Nonnull;
+import java.io.Serializable;
 
 /**
  * Wraps a string containing a cleartext password.
@@ -12,7 +13,7 @@ import javax.annotation.Nonnull;
  * @author This source is copyright <a href="http://www.databasesandlife.com">Adrian Smith</a> and licensed under the LGPL 3.
  * @see <a href="https://github.com/adrianmsmith/databasesandlife-java-common">Project on GitHub</a>
  */
-public class CleartextPassword {
+public class CleartextPassword implements Serializable {
 
     public final @Nonnull String cleartext;
 
