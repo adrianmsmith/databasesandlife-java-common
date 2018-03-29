02c8baf6cbda54ca43823e745c3913bc401d975c
Adrian Smith <adrian.m.smith@gmail.com>
2018-03-29 17:55:39 +0200

    Add deprecation notice to one class

--- src/main/java/com/databasesandlife/util/Coalescor.java
+++ src/main/java/com/databasesandlife/util/Coalescor.java
@@ -1,6 +1,8 @@
 package com.databasesandlife.util;
 
 /**
+ * @deprecated Use {@link java.util.Optional} instead, as its {@link java.util.Optional#orElse(Object)} method
+ *     returns a non-null object that the IntelliJ can verify as non-null.
  * @author This source is copyright <a href="http://www.databasesandlife.com">Adrian Smith</a> and licensed under the LGPL 3.
  * @see <a href="https://github.com/adrianmsmith/databasesandlife-java-common">Project on GitHub</a>
  */
