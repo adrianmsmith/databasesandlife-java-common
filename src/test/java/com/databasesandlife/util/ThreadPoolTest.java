cf4bae1f9d0c55c887ba5f5d3ff6baebe7b1e273
Adrian Smith <adrian.m.smith@gmail.com>
2018-06-27 11:07:53 +0200

    Remove speed test for ThreadPoolTest which was unreliable

--- src/test/java/com/databasesandlife/util/ThreadPoolTest.java
+++ src/test/java/com/databasesandlife/util/ThreadPoolTest.java
@@ -1,7 +1,5 @@
 package com.databasesandlife.util;
 
-import com.google.common.base.Stopwatch;
-
 import junit.framework.TestCase;
 
 /**
@@ -9,37 +7,7 @@ import junit.framework.TestCase;
  * @see <a href="https://github.com/adrianmsmith/databasesandlife-java-common">Project on GitHub</a>
  */
 public class ThreadPoolTest extends TestCase {
-    
-    public void test() {
-        int threadCount = 2;
-        int taskCount = 10;
-        int durationMillis = 100;
-        
-        ThreadPool pool = new ThreadPool();
-        pool.setThreadCount(threadCount);
-        Runnable sleepTask = new Runnable() {
-            @Override public void run() {
-                try { Thread.sleep(durationMillis); }
-                catch (InterruptedException e) { }
-            }
-        };
-        Runnable seedTask = new Runnable() {
-            @Override public void run() {
-                for (int i = 0 ; i < taskCount; i++)
-                    pool.addTask(sleepTask);
-            };
-        };        
-        pool.addTask(seedTask);
 
-        Stopwatch stopwatch = new Stopwatch().start();
-        pool.execute();
-        long elapsedMillis = stopwatch.elapsedMillis();
-        long expectedMillis = taskCount * durationMillis / threadCount;
-        String msg = "expectedMillis="+expectedMillis+", elapsedMillis="+elapsedMillis;
-        assertTrue(msg, elapsedMillis > 0.9*expectedMillis);
-        assertTrue(msg, elapsedMillis < 1.5*expectedMillis);
-    }
-    
     public void testException() {
         ThreadPool pool = new ThreadPool();
         pool.addTask(new Runnable() {
