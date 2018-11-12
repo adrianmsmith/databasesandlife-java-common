a6dfd0d7c333463c7bb90441206f9367c4ecd36f
Adrian Smith <adrian.m.smith@gmail.com>
2018-11-12 14:59:22 +0100

    6.2.0: Backport from 7.2.0: Add CachingFutureModel

---
+++ src/main/java/com/databasesandlife/util/wicket/CachingFutureModel.java
@@ -0,0 +1,38 @@
+package com.databasesandlife.util.wicket;
+
+import com.databasesandlife.util.Future;
+import org.apache.wicket.model.AbstractReadOnlyModel;
+
+import javax.annotation.CheckForNull;
+import javax.annotation.Nonnull;
+import java.io.Serializable;
+
+/**
+ * Can be used simple as a cache of a Future, or also as a Wicket model.
+ * <p>
+ * Futures have the problem that they are not serializable.
+ * Wicket only needs to serialize things after they've been displayed, i.e. after the future has delivered its results.
+ * </p>
+ * @param <T>
+ */
+abstract public class CachingFutureModel<T extends Serializable> extends AbstractReadOnlyModel<T> {
+    
+    @CheckForNull protected transient final Future<T> future;
+    @CheckForNull protected T contents;
+
+    public CachingFutureModel() {
+        future = new Future<T>() {
+            @Override protected T populate() {
+                return CachingFutureModel.this.populate();
+            }
+        };
+    }
+
+    abstract protected @Nonnull T populate();
+
+    @Override public T getObject() {
+        if (future != null) contents = future.get();
+        if (contents != null) return contents;
+        throw new RuntimeException("Object serialized before future executed");
+    }
+}
