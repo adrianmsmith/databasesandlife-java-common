807e5e6e14aca19aabd2611289a00e7b4b7680bf
Adrian Smith <adrian.m.smith@gmail.com>
2020-05-21 14:27:47 +0200

    11.0.0: Improve performance of Wicket's MapKeyModel

--- src/main/java/com/databasesandlife/util/wicket/MapKeyModel.java
+++ src/main/java/com/databasesandlife/util/wicket/MapKeyModel.java
@@ -2,39 +2,37 @@ package com.databasesandlife.util.wicket;
 
 import org.apache.wicket.model.AbstractWrapModel;
 import org.apache.wicket.model.IModel;
-import org.apache.wicket.model.PropertyModel;
 
 import javax.annotation.Nonnull;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-
-import static java.util.stream.Collectors.toList;
+import java.util.SortedMap;
 
 /**
- * Takes an underlying model which returns a map, and returns that map's keys
+ * Takes an underlying model which returns a map, and returns that map's keys.
+ *   <p>
+ *       <b>Performance note:</b> 
+ *       Do not use this in a ListView if there are a large number of entries (e.g. more than 1,000).
+ *       Unfortunately the model's getObject method gets called a great number of times if it's used as a ListView's model,
+ *       and the populateItem method calls getModelObject, which does a listView.getModel().get(i).
+ *   </p>
  *
  * @author This source is copyright <a href="http://www.databasesandlife.com">Adrian Smith</a> and licensed under the LGPL 3.
  * @see <a href="https://github.com/adrianmsmith/databasesandlife-java-common">Project on GitHub</a>
  */
 public class MapKeyModel<K extends Comparable<?>> extends AbstractWrapModel<List<K>> {
 
-    final @Nonnull IModel<Map<K,?>> wrappedModel;
+    final @Nonnull IModel<SortedMap<K,?>> wrappedModel;
 
-    public MapKeyModel(@Nonnull IModel<Map<K,?>> wrappedModel) {
+    public MapKeyModel(@Nonnull IModel<SortedMap<K,?>> wrappedModel) {
         this.wrappedModel = wrappedModel;
     }
 
-    /** Convenience method which wraps a {@link org.apache.wicket.model.PropertyModel} */
-    public MapKeyModel(@Nonnull Object modelObject, @Nonnull String expression) {
-        this(new PropertyModel<>(modelObject, expression));
-    }
-
-    @Override public IModel<Map<K,?>> getWrappedModel() {
+    @Override public IModel<SortedMap<K,?>> getWrappedModel() {
         return wrappedModel;
     }
 
     @Override public List<K> getObject() {
-        return getWrappedModel().getObject().keySet().stream().sorted().collect(toList());
+        return new ArrayList<>(getWrappedModel().getObject().keySet());
     }
 }
